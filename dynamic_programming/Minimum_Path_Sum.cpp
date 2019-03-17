e234a1f6f4cd6f0c26c841af61e9d3df879b4ca8
julyjohn <yzjhhe@outlook.com>
2019-03-17 12:43:13 +0800

    64. Minimum Path Sum  note: 矩阵的最小路径和  一维dp改进

--- dynamic_programming/Minimum_Path_Sum.cpp
+++ dynamic_programming/Minimum_Path_Sum.cpp
@@ -26,6 +26,34 @@ Explanation: Because the path 1→3→1→1→1 minimizes the sum.
 
 class Solution {
 public:
+
+    // 一维dp改进
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n;
+        if(m < 1){
+            return 0;
+        }else{
+            n = grid[0].size();
+            if(n < 1){
+                return 0;
+            }
+        }
+        vector<int > dp(n, 0);
+        for(int i = 0; i < m; ++i){
+            for(int j = 0; j < n; ++j){
+                if(j == 0){
+                    dp[j] = grid[i][j] + dp[j];
+                }else if(i == 0){
+                    dp[j] = grid[i][j] + dp[j-1];
+                }else{
+                    dp[j] = grid[i][j] + min(dp[j-1], dp[j]);
+                }
+            }
+        }
+        return dp[n-1];
+    }
+
     int minPathSum(vector<vector<int>>& grid) {
         int n = grid.size();
         int m;
