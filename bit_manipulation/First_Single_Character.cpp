252a5fd07449343df9e2a6d6ce2d795ab812a625
julyjohn <yzjhhe@outlook.com>
2019-04-03 16:29:26 +0800

    字符串中第一次出现的字符
    常规方法

--- bit_manipulation/First_Single_Character.cpp
+++ bit_manipulation/First_Single_Character.cpp
@@ -10,26 +10,56 @@
  * */
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
+
 class Solution {
 public:
+    // 常规方法
     int FirstNotRepeatingChar(string str) {
-        int* bitmap = new int[16]();
-        int arr_idx, bit_idx;
-        for(int i = 0; i < str.length(); ++i){
-            arr_idx = str[i] * 2 / 32;
-            bit_idx = str[i] * 2 % 32;
-            if(((bitmap[arr_idx] >> bit_idx) & 1) == 0){
-                bitmap[arr_idx] |= (1 << bit_idx);
-            }else if(((bitmap[arr_idx] >> (bit_idx+1)) & 1) == 0){
-                bitmap[arr_idx] |= (1 << (bit_idx+1));
+        vector<short> bitmap(52, 0);
+        int n = str.length();
+        for (int i = 0; i < n; ++i) {
+            short idx;
+            if (str[i] >= 'a' && str[i] <= 'z') {
+                idx = str[i] - 'a';
+            } else {
+                idx = str[i] - 'A' + 26;
+            }
+            bitmap[idx] += 1;
+        }
+        for (int i = 0; i < n; ++i) {
+            short idx;
+            if (str[i] >= 'a' && str[i] <= 'z') {
+                idx = str[i] - 'a';
+            } else {
+                idx = str[i] - 'A' + 26;
+            }
+            if (bitmap[idx] == 1) {
+                return i;
             }
         }
-        for(int i = 0; i < str.length(); ++i){
+        return -1;
+    }
+
+    // 位运算
+    int FirstNotRepeatingChar1(string str) {
+        int *bitmap = new int[16]();
+        int arr_idx, bit_idx;
+        for (int i = 0; i < str.length(); ++i) {
             arr_idx = str[i] * 2 / 32;
             bit_idx = str[i] * 2 % 32;
-            if(((bitmap[arr_idx] >> bit_idx) & 1) == 1 && ((bitmap[arr_idx] >> (bit_idx+1)) & 1) == 0){
+            if (((bitmap[arr_idx] >> bit_idx) & 1) == 0) {
+                bitmap[arr_idx] |= (1 << bit_idx);
+            } else if (((bitmap[arr_idx] >> (bit_idx + 1)) & 1) == 0) {
+                bitmap[arr_idx] |= (1 << (bit_idx + 1));
+            }
+        }
+        for (int i = 0; i < str.length(); ++i) {
+            arr_idx = str[i] * 2 / 32;
+            bit_idx = str[i] * 2 % 32;
+            if (((bitmap[arr_idx] >> bit_idx) & 1) == 1 && ((bitmap[arr_idx] >> (bit_idx + 1)) & 1) == 0) {
                 return i;
             }
         }
@@ -37,9 +67,9 @@ public:
     }
 };
 
-int main(){
+int main() {
     Solution *solution = new Solution();
     string str = "google";
-    solution->FirstNotRepeatingChar(str);
+    cout << solution->FirstNotRepeatingChar(str);
     return 0;
 }
